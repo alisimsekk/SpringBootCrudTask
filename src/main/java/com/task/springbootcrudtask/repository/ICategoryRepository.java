203ac87fd7e1a3e3c33acd8d7c3f3cd4c5673bc8
alisimsek <ali.simsek035@gmail.com>
2022-12-16 22:04:44 +0300

    Repository layer was created

---
+++ src/main/java/com/task/springbootcrudtask/repository/ICategoryRepository.java
@@ -0,0 +1,9 @@
+package com.task.springbootcrudtask.repository;
+
+import com.task.springbootcrudtask.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICategoryRepository extends JpaRepository<Category,Integer> {
+}
